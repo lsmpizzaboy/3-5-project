caf7a18253d691ccf375512d649582bab1f6dce5
lsmpizzaboy <lsm66884985@gmail.com>
2026-07-26 19:55:28 +0900

    Update utils.py

--- AI-Study-Timer/utils.py
+++ AI-Study-Timer/utils.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from supabase import create_client, Client
 
+@st.cache_resource
+def init_supabase() -> Client:
+    # secrets.toml에 저장해둔 값들을 불러옵니다.
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["key"]
+    return create_client(url, key)

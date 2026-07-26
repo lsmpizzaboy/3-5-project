5160d9f315596dab1ddba4621ee1a3eed928de41
lsmpizzaboy <lsm66884985@gmail.com>
2026-07-26 22:46:40 +0900

    Update app.py

--- AI-Study-Timer/app.py
+++ AI-Study-Timer/app.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="AI 맞춤형 학습 타이머", page_icon="⏱️", layout="wide")
 
-st.title("AI 피드백 학습 타이머에 오신 것을 환영합니다!")
+st.title("AI 피드백 학습 타이머")
 st.markdown("""
-이 프로젝트는 **제미나이(Gemini) API**를 활용하여 만들어졌습니다.
+이 프로그램은**제미나이(Gemini) API**를 활용하여 만들었습니다.
 
-👈 좌측 사이드바 메뉴를 이용하세요.
-
+👈 좌측 사이드바 메뉴를 통해 이동하세요.
+ **로그인 및 회원가입**을 먼저 진행하세요.
+""")
 
 # 사용자 정보 전역 세션 초기화
 if "logged_in_user" not in st.session_state:
